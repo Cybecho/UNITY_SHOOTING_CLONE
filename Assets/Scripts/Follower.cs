2c12d669d37d495b046287a0fab10a5d47823406
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-09-12 20:36:09 +0900

    Update Player class to add followers

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -29,16 +29,29 @@ public class Follower : MonoBehaviour
 
     void Fire()
     {
-        
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        GameObject bullet = Instantiate(bulletObj, transform.position, transform.rotation); // 총알 오브젝트 생성
+        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>(); // 총알 오브젝트의 Rigidbody2D 컴포넌트 가져오기
+        rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse); // 위쪽으로 힘을 가함
+
+        curShotDelay = 0; // 현재 총알 발사 딜레이 초기화
     }
 
     void AutoFire()
     {
-        
+        curShotDelay += Time.deltaTime; // 현재 총알 발사 딜레이에 시간을 더함
+
+        if (curShotDelay >= maxShotDelay)
+        {
+            Fire(); // 발사 함수 호출
+            curShotDelay = 0; // 현재 총알 발사 딜레이 초기화
+        }
     }
 
     void Reload()
     {
-        
+        curShotDelay += Time.deltaTime; // 현재 총알 발사 딜레이에 시간을 더함
     }
 }
